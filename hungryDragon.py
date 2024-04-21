12dad35e74777ba0ebf5ea8eb22c83ea1d6bf137
ricardorosa <rjrosa@outlook.com>
2024-04-21 18:16:31 +0100

    Initial commit adding the game assets and screen initial displays.

---
+++ hungryDragon.py
@@ -0,0 +1,80 @@
+import pygame, random
+
+#Initialize pygame
+pygame.init()
+
+#Set display surface
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 400
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Hungry dragon")
+
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+
+#Set game values
+PLAYER_STARTING_LIVES = 5
+PLAYER_VELOCITY = 10
+COIN_STARTING_VELOCITY = 10
+COIN_ACCELERATION = .5
+BUFFER_DISTANCE = 100
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+
+#Set colors
+GREEN = (0, 255, 0)
+DARKGREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+#Set fonts
+font = pygame.font.Font('Hungry _Dragon_assets/AttackGraffiti.ttf', 32)
+
+#Set text
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+title_text = font.render("Hungry dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH//2
+title_rect.y = 10
+
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+game_over_text = font.render("GAMEOVER", True, GREEN, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+continue_text = font.render("Press a key to play again", True, GREEN, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
+
+#The main game loop
+running = True
+while running:
+    #Check to see if user wants to quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the display
+    display_surface.fill(BLACK)
+
+    # Blit the HUD to screen
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    pygame.draw.line(display_surface, WHITE, (0, 64), (WINDOW_WIDTH, 64), 2)
+
+
+    # Update display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
+#End the game
+pygame.quit()
