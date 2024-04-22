f3af7a4d45c904dd7bd536304f5a46613ddf89f9
ricardorosa <rjrosa@outlook.com>
2024-04-22 10:33:20 +0100

    introducing levels and game states

--- hungryDragon.py
+++ hungryDragon.py
@@ -21,8 +21,8 @@ COIN_ACCELERATION = .5
 BUFFER_DISTANCE = 100
 
 score = 0
-player_lives = PLAYER_STARTING_LIVES
-coin_velocity = COIN_STARTING_VELOCITY
+playerLives = PLAYER_STARTING_LIVES
+coinVelocity = COIN_STARTING_VELOCITY
 
 #Set colors
 GREEN = (0, 255, 0)
@@ -34,45 +34,48 @@ BLACK = (0, 0, 0)
 font = pygame.font.Font('Hungry_Dragon_assets/AttackGraffiti.ttf', 32)
 
 #Set text
-score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
-score_rect = score_text.get_rect()
-score_rect.topleft = (10, 10)
+scoreText = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+scoreRect = scoreText.get_rect()
+scoreRect.topleft = (10, 10)
 
-title_text = font.render("Hungry dragon", True, GREEN, WHITE)
-title_rect = title_text.get_rect()
-title_rect.centerx = WINDOW_WIDTH//2
-title_rect.y = 10
+titleText = font.render("Hungry dragon", True, GREEN, WHITE)
+titleRect = titleText.get_rect()
+titleRect.centerx = WINDOW_WIDTH // 2
+titleRect.y = 10
 
-lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
-lives_rect = lives_text.get_rect()
-lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+livesText = font.render("Lives: " + str(playerLives), True, GREEN, DARKGREEN)
+livesRect = livesText.get_rect()
+livesRect.topright = (WINDOW_WIDTH - 10, 10)
 
-game_over_text = font.render("GAMEOVER", True, GREEN, DARKGREEN)
-game_over_rect = game_over_text.get_rect()
-game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+gameOverText = font.render("GAMEOVER", True, GREEN, DARKGREEN)
+gameOverRect = gameOverText.get_rect()
+gameOverRect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
 
-continue_text = font.render("Press a key to play again", True, GREEN, DARKGREEN)
-continue_rect = continue_text.get_rect()
-continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
+continueText = font.render("Press a key to play again", True, GREEN, DARKGREEN)
+continueRect = continueText.get_rect()
+continueRect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 32)
 
 
 #Set sounds and music
-coin_sound = pygame.mixer.Sound("Hungry_Dragon_assets/coin_sound.wav")
-miss_sound = pygame.mixer.Sound("Hungry_Dragon_assets/miss_sound.wav")
-miss_sound.set_volume(.1)
+coinSound = pygame.mixer.Sound("Hungry_Dragon_assets/coin_sound.wav")
+missSound = pygame.mixer.Sound("Hungry_Dragon_assets/miss_sound.wav")
+missSound.set_volume(.1)
 pygame.mixer.music.load("Hungry_Dragon_assets/ftd_background_music.wav")
 
 #Set images
-player_image = pygame.image.load("Hungry_Dragon_assets/dragon_right.png")
-player_rect = player_image.get_rect()
-player_rect.left = 32
-player_rect.centery = WINDOW_HEIGHT//2
+playerImage = pygame.image.load("Hungry_Dragon_assets/dragon_right.png")
+playerRect = playerImage.get_rect()
+playerRect.left = 32
+playerRect.centery = WINDOW_HEIGHT // 2
 
-coin_image = pygame.image.load("Hungry_Dragon_assets/coin.png")
-coin_rect = coin_image.get_rect()
-coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
-coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+coinImage = pygame.image.load("Hungry_Dragon_assets/coin.png")
+coinRect = coinImage.get_rect()
+coinRect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+coinRect.y = random.randint(64, WINDOW_HEIGHT - 32)
 
+# Add levels
+LEVELS = [(10, 10), (15, 12), (20, 15)]  # Format: (COIN_VELOCITY, COIN_ACCELERATION)
+currentLevel = 0
 
 # Game functionality
 
@@ -87,69 +90,71 @@ def moveDragon():
     keys = pygame.key.get_pressed()
 
     # Check if the up arrow key is pressed and the dragon's top position is above the top boundary
-    if keys[pygame.K_UP] and player_rect.top > 64:
-        player_rect.y -= PLAYER_VELOCITY  # Move the dragon up by PLAYER_VELOCITY units
+    if keys[pygame.K_UP] and playerRect.top > 64:
+        playerRect.y -= PLAYER_VELOCITY  # Move the dragon up by PLAYER_VELOCITY units
 
     # Check if the down arrow key is pressed and the dragon's bottom position is below the bottom boundary
-    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
-        player_rect.y += PLAYER_VELOCITY  # Move the dragon down by PLAYER_VELOCITY units
+    if keys[pygame.K_DOWN] and playerRect.bottom < WINDOW_HEIGHT:
+        playerRect.y += PLAYER_VELOCITY  # Move the dragon down by PLAYER_VELOCITY units
 
     return True  # Return True to indicate that the function executed successfully
 
 
 def resetCoin():
     # Reset the  x-position of the coin outside the right edge of the window
-    coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+    coinRect.x = WINDOW_WIDTH + BUFFER_DISTANCE
     # Set the y-position of the coin to a random value within the window area
-    coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+    coinRect.y = random.randint(64, WINDOW_HEIGHT - 32)
 
 
 def updateGame():
     # Grab global variables to update them in the function
-    global score, player_lives, coin_velocity
+    global score, playerLives, coinVelocity, currentLevel
 
     # Check if the coin has reached the left edge of the window
-    if coin_rect.x < 0:
+    if coinRect.x < 0:
         # Decrease player's lives count when the coin is missed
-        player_lives -= 1
+        playerLives -= 1
         # Play the miss sound effect
-        miss_sound.play()
+        missSound.play()
         # Reset the coin to a new random position
         resetCoin()
     else:
         # Move the coin towards the left side of the window
-        coin_rect.x -= coin_velocity
+        coinRect.x -= LEVELS[currentLevel][0]
 
     # Check if there's collision between the player and the coin
-    if player_rect.colliderect(coin_rect):
+    if playerRect.colliderect(coinRect):
         # Increase player's score when the coin is caught
         score += 1
         # Play the coin collection sound effect
-        coin_sound.play()
+        coinSound.play()
         # Increase the coin velocity to increase difficulty
-        coin_velocity += COIN_ACCELERATION
+        coinVelocity += LEVELS[currentLevel][1]
         # Reset the coin to a new random position
         resetCoin()
+    if score == 10 and currentLevel < len(LEVELS) - 1:
+        currentLevel += 1
 
 def showGameOver():
     # Blit the "GAME OVER" text to the screen
-    display_surface.blit(game_over_text, game_over_rect)
+    display_surface.blit(gameOverText, gameOverRect)
     # Blit the "Press any key to play again" to the screen
-    display_surface.blit(continue_text, continue_rect)
+    display_surface.blit(continueText, continueRect)
     # Update to show the game over screen
     pygame.display.update()
 
 
 def gameReseter():
     # Grabbing global variables to use them in the function
-    global score, player_lives, coin_velocity
+    global score, playerLives, coinVelocity, currentLevel
 
     # Reset the game variables to their initial values
     score = 0  # Reset the player's score to zero
-    player_lives = PLAYER_STARTING_LIVES  # Reset the player's lives to the original value
-    player_rect.y = WINDOW_HEIGHT // 2  # Reset the player's vertical position to the center of the window
-    coin_velocity = COIN_STARTING_VELOCITY  # Reset the coin velocity to the original value
-
+    playerLives = PLAYER_STARTING_LIVES  # Resting the player's lives
+    playerRect.y = WINDOW_HEIGHT // 2  # Resting the player's vertical position
+    coinVelocity = COIN_STARTING_VELOCITY  # Resting the coin velocity
+    currentLevel = 0 # Resting the current level
     # Restart the background music
     pygame.mixer.music.play(-1, 0.0)
 
@@ -172,6 +177,12 @@ def gameOverHandler():
     # return True to continue the game loop
     return True
 
+# Define game states
+MENU = 0
+PLAYING = 1
+GAME_OVER = 2
+gameState = PLAYING
+
 
 # Main game loop
 pygame.mixer.music.play(-1, 0.0)
@@ -179,14 +190,21 @@ running = True
 while running:
     # Check for user input
     running = moveDragon()  # update game accordingly
-    updateGame()  # update the game accordingly
+    if gameState == PLAYING:  # If the game state is playing
+        updateGame()  # update the game accordingly
+        if playerLives == 0:  # If player has no lives left
+            gameState = GAME_OVER  # Set game state to game over
+    elif gameState == GAME_OVER:  # If the game state is game over
+        showGameOver()  # Display the game over
+        if not gameOverHandler():  # If the player chooses to quit
+            running = False  # Exit the game loop
 
     # Render game elements on the screen
-    score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)  # score text
-    lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)  # lives text
+    scoreText = font.render("Score: " + str(score), True, GREEN, DARKGREEN)  # score text
+    livesText = font.render("Lives: " + str(playerLives), True, GREEN, DARKGREEN)  # lives text
 
     # Check player lives for game over
-    if player_lives == 0:
+    if playerLives == 0:
         gameOverHandler()  # Display game over
         # Handle game over to continue the game
         if not gameOverHandler():
@@ -195,12 +213,12 @@ while running:
     # Fill the background
     display_surface.fill(BLACK)
     # Blit game elements
-    display_surface.blit(score_text, score_rect)  # Render score text
-    display_surface.blit(title_text, title_rect)  # Render title text
-    display_surface.blit(lives_text, lives_rect)  # Render lives text
+    display_surface.blit(scoreText, scoreRect)  # Render score text
+    display_surface.blit(titleText, titleRect)  # Render title text
+    display_surface.blit(livesText, livesRect)  # Render lives text
     pygame.draw.line(display_surface, WHITE, (0, 64), (WINDOW_WIDTH, 64), 2)  # Draw a line
-    display_surface.blit(player_image, player_rect)  # Render the player
-    display_surface.blit(coin_image, coin_rect)  # Render the coin
+    display_surface.blit(playerImage, playerRect)  # Render the player
+    display_surface.blit(coinImage, coinRect)  # Render the coin
 
     pygame.display.update()  # Update the display
     clock.tick(FPS)  # Cap the frame rate
